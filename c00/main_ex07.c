a3e84b93478a43c9f65d004f498a5fba85530899
hubble <38990435+hubble42@users.noreply.github.com>
2022-08-28 20:43:38 +0200

    Create main_ex07.c

---
+++ c00/main_ex07.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "ex07/ft_putnbr.c"
+int main (void)
+{
+	ft_putnbr(4563);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	ft_putnbr(5);
+	ft_putchar('\n');
+
+}
